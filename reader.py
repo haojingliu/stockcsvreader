febeb0ae682306278f3035593f7cb214b43d9c38
Haojing Liu <haojing.liu@gmail.com>
2016-02-24 12:21:56 +0800

    Reading from csv to sql

---
+++ reader.py
@@ -0,0 +1,35 @@
+import csv
+
+with open('stock.csv', mode='r') as infile:
+    reader = csv.reader(infile)
+    table = 'stock'
+    with open('stock.sql', mode='w') as outfile:
+        outfile.write('''
+    create table stock(
+        confirm_no int,
+        order_no varchar(100),
+        trade_date varchar(20),
+        `type` varchar(2),
+        security varchar(10),
+        `unit` int,
+        avg_price float(20,4),
+        brokerage float(20,4),
+        net_proceeds float(20,4),
+        settlement_date varchar(120),
+        confirmation_status varchar(10)
+    );
+        ''');
+        # Skip title.
+        rows = iter(reader)
+        next(rows)
+
+        for row in rows:
+            query = 'insert into ' + table + ' values ('+ ','.join('"{0}"'.format(w) for w in row[0:-1])+');\n'
+            outfile.write(query)
+        # mydict = {rows[0]:rows[1] for rows in reader}
+    # print row for row in reader
+    # d = {}
+    # for row in reader:
+    #    # k, v = row
+    #    # d[k] = v
+    #    print row
